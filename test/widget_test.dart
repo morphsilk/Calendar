6fe29c4bae79790e399373fdb730acdea832a3ac
Dobrynin Artemy <dobrolygin@xmail.ru>
2025-02-21 14:29:21 +0500

    changed widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:mood_calendar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // Инициализация моковых SharedPreferences
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
 
-    // Verify that our counter starts at 0.
+    // Запуск тестируемого приложения
+    await tester.pumpWidget(MoodCalendarApp(prefs: prefs));
+
+    // Проверка начального состояния
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Нажатие на кнопку '+'
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // Проверка увеличенного значения
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
